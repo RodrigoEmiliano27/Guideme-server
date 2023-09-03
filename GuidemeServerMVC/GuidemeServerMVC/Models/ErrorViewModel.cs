ddc8dc3572a68cf19253e6d5a176a14ea58069bf
lucasscbr <55152520+lucasscbr@users.noreply.github.com>
2023-09-03 12:59:17 -0300

    Delete GuidemeServerMVC directory

--- GuidemeServerMVC/GuidemeServerMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GuideMeServerMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
