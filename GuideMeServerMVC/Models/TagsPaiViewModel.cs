6a6991c45d62ff9337f5e20503d8ed8f1cd65dbf
RodrigoEmiliano27 <54250496+RodrigoEmiliano27@users.noreply.github.com>
2023-09-10 14:00:43 -0300

    Incluido view models no contexto do banco de dados

---
+++ GuideMeServerMVC/Models/TagsPaiViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuideMeServerMVC.Models
+{
+    [Table("tbTagsPai")]
+    public class TagsPaiViewModel
+    {
+        [Column("ID_TAG")]
+        public int Id_Tag { get; set; }
+        [Column("IDTAG_PAI")]
+        public int Id_Tag_Pai { get; set; }
+    }
+}
