121f26b0fe5d1dbabff9e5bbe3b29f764cb8d24b
Lucas Santos Costa <55152520+lucasscbr@users.noreply.github.com>
2023-10-01 21:23:00 -0300

    Listagem Tags por estabelecimento

--- GuideMeServerMVC/Controllers/TagController.cs
+++ GuideMeServerMVC/Controllers/TagController.cs
@@ -44,7 +44,7 @@ namespace GuideMeServerMVC.Controllers
                 if (tagInfo == null)
                     return Ok("Not Found");
 
-                List<TagViewModel> listaTags = 
+                List<TagViewModel> listaTags =
                     _context.Tags.Where(x => x.EstabelecimentoId == tagInfo.EstabelecimentoId).ToList();
 
                 List<LugaresViewModel> lista = new List<LugaresViewModel>();
@@ -60,7 +60,7 @@ namespace GuideMeServerMVC.Controllers
                 TagsDataTO data = new TagsDataTO();
                 data.Tags = listaTags;
                 data.Lugares = lista;
-                    
+
 
                 return Ok(data);
             }
@@ -98,23 +98,62 @@ namespace GuideMeServerMVC.Controllers
                     tagmodel.Nome = tagTO.TagName;
                     tagmodel.tipoTag = (int)EnumTipoTag.NaoCadastrada;
                     tagmodel.TagId = tagTO.TagID;
-                    tagmodel.EstabelecimentoId=tagTO.IdEstabelecimento;
+                    tagmodel.EstabelecimentoId = tagTO.IdEstabelecimento;
 
                     await _context.AddAsync(tagmodel);
                     await _context.SaveChangesAsync();
 
                     return Ok(JsonConvert.SerializeObject(tagmodel));
                 }
-             
+
             }
             catch (Exception err)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError,err.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, err.ToString());
             }
 
             return BadRequest();
         }
 
-        
+        [HttpGet("ExibirTagsEstabelecimento")]
+        public IActionResult ExibirTagsEstabelecimento()
+        {
+            Debug.WriteLine("Listando Tags");
+            var user = _context.UsuariosEstabelecimento.FirstOrDefault(o => o.Id == HttpContext.Session.GetInt32("UserId"));
+            var tags = _context.Tags.Where(o => o.EstabelecimentoId == user.Id_Estabelecimento).ToList();
+
+
+            return View("TagsEstabelecimento", tags);
+        }
+
+        /*[HttpPost]
+        public IActionResult Editar(int id, string novoNome)
+        {
+            var tag = _context.Tags.FirstOrDefault(o => o.Id == id);
+            tag.Nome = novoNome;
+            _context.Tags.Update(tag);
+            _context.SaveChanges();
+
+            return ExibirTagsEstabelecimento();
+            // Lógica para atualizar a tag com o novo nome
+            // Use o id para identificar a tag a ser atualizada
+            // Atualize o registro no banco de dados ou em outra fonte de dados
+
+            // Retorne um JSON ou uma resposta apropriada para indicar o sucesso ou falha da atualização
+        }*/
+
+        [HttpPost]
+        public IActionResult Editar(String tag)
+        {
+            Debug.WriteLine("tag => " + tag);
+            /*var tag = _context.Tags.FirstOrDefault(o => o.Id == id);
+            tag.Nome = novoNome;
+            _context.Tags.Update(tag);
+            _context.SaveChanges();*/
+            
+            return ExibirTagsEstabelecimento();
+
+        }
+
     }
 }
